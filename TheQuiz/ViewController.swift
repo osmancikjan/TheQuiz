a2ce671e4398b4f32d3905abc765e0bfa6be09ec
Jan Osmancik <j.osmancik@gmail.com>
2024-03-22 21:27:47 +0100

    Initial Commit

---
+++ TheQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheQuiz
+//
+//  Created by Jan Osmančík on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
