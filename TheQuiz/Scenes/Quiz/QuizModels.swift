4ac13330e4e26993c3f740d1a64c27edbb0a5330
Jan Osmancik <j.osmancik@gmail.com>
2024-03-24 23:09:42 +0100

    codable model support

--- TheQuiz/Scenes/Quiz/QuizModels.swift
+++ TheQuiz/Scenes/Quiz/QuizModels.swift
@@ -21,7 +21,7 @@ enum Quiz {
     }
 }
 
-struct QuizModel: Equatable {
+class QuizModel: Equatable, Codable {
     static func == (lhs: QuizModel, rhs: QuizModel) -> Bool {
         lhs.id == rhs.id && lhs.title == rhs.title && lhs.questions == rhs.questions
     }
@@ -29,27 +29,123 @@ struct QuizModel: Equatable {
     let id: String
     let title: String
     var questions: [QuestionModel]
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case title
+        case questions
+    }
+    
+    init(id: String, title: String, questions: [QuestionModel]) {
+        self.id = id
+        self.title = title
+        self.questions = questions
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        questions = try container.decode([QuestionModel].self, forKey: .questions)
+    }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            
+            try container.encode(id, forKey: .id)
+            try container.encode(title, forKey: .title)
+            try container.encode(questions, forKey: .questions)
+        }
+
 }
 
-struct QuestionModel: Equatable {
+class QuestionModel: Equatable, Codable {
+    static func == (lhs: QuestionModel, rhs: QuestionModel) -> Bool {
+        lhs.id == rhs.id && lhs.text == rhs.text && lhs.allowsMultipleAnswers == rhs.allowsMultipleAnswers && lhs.answers == rhs.answers
+    }
+    
     let id: String
     let text: String
-    var answered: Bool = false
     let allowsMultipleAnswers: Bool
     var answers: [AnswerModel]
-    var selectedAnswers: [AnswerModel] = []
     
-    mutating func deselectAllAnswers() {
+    func deselectAllAnswers() {
         for index in answers.indices {
             answers[index].selected = false
         }
     }
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case text
+        case allowsMultipleAnswers
+        case answers
+    }
+    
+    init(id: String, text: String, allowsMultipleAnswers: Bool, answers: [AnswerModel]) {
+        self.id = id
+        self.text = text
+        self.allowsMultipleAnswers = allowsMultipleAnswers
+        self.answers = answers
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(String.self, forKey: .id)
+        text = try container.decode(String.self, forKey: .text)
+        allowsMultipleAnswers = try container.decode(Bool.self, forKey: .allowsMultipleAnswers)
+        answers = try container.decode([AnswerModel].self, forKey: .answers)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(text, forKey: .text)
+        try container.encode(allowsMultipleAnswers, forKey: .allowsMultipleAnswers)
+        try container.encode(answers, forKey: .answers)
+    }
 }
 
-struct AnswerModel: Equatable {
+class AnswerModel: Equatable, Codable {
+    static func == (lhs: AnswerModel, rhs: AnswerModel) -> Bool {
+        lhs.id == rhs.id && lhs.text == rhs.text && lhs.selected == rhs.selected && lhs.correct == rhs.correct
+    }
+    
     let id: String
     let text: String
     var selected: Bool = false 
     let correct: Bool
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case text
+        case selected
+        case correct
+    }
+    
+    init(id: String, text: String, selected: Bool? = false, correct: Bool) {
+        self.id = id
+        self.text = text
+        self.selected = selected ?? false
+        self.correct = correct
+    }
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.text = try container.decode(String.self, forKey: .text)
+        self.selected = try container.decode(Bool.self, forKey: .selected)
+        self.correct = try container.decode(Bool.self, forKey: .correct)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(text, forKey: .text)
+        try container.encode(selected, forKey: .selected)
+        try container.encode(correct, forKey: .correct)
+    }
 }
 
--- TheQuizTests/Quiz/QuizInteractorTests.swift
+++ TheQuizTests/Quiz/QuizInteractorTests.swift
@@ -6,30 +6,34 @@
 //
 
 import XCTest
+@testable import TheQuiz 
 
-final class QuizInteractorTests: XCTestCase {
+class MockQuizInteractorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGetAnswersCount() {
+        // Given
+        let interactor = MockQuizInteractor()
+        interactor.stubbedGetAnswersCountResult = 2
+
+        // When
+        let answersCount = interactor.getAnswersCount()
+
+        // Then
+        XCTAssertEqual(answersCount, 2)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testGetAnswer() {
+        // Given
+        let interactor = MockQuizInteractor()
+        let expectedAnswer = AnswerModel(id: "1", text: "Answer 1", selected: false, correct: true)
+        interactor.stubbedGetAnswerResult = expectedAnswer
+
+        // When
+        let answer = interactor.getAnswer(for: 0)
+
+        // Then
+        XCTAssertEqual(answer, expectedAnswer)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // Add more test cases for other methods as needed...
 }
